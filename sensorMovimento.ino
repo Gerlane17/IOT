a522a1a4be8aecd77e76d967e2f37f52f0d0ea4a
Gerlane17 <gerlanefev2020@gmail.com>
2024-10-01 09:17:36 -0300

    IOT sensor de movimento com barulho

---
+++ sensorMovimento.ino
@@ -0,0 +1,19 @@
+int sensorPIR = 3;
+int buzzer = 5;
+
+
+void setup()
+{
+ pinMode(sensorPIR, INPUT);
+ pinMode(buzzer,OUTPUT);
+}
+
+void loop()
+{
+ int detectarPresenca = digitalRead(sensorPIR);
+  if(detectarPresenca == 1){
+  	tone(buzzer,264);
+  }else{
+  	noTone(buzzer);
+  }
+}
